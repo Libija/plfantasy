7246976e61c1aa742c620f208b6a6813539cdd9f
Libija <emrah_libic123@hotmail.com>
2025-07-07 10:40:19 +0200

    baza

--- main.py
+++ main.py
@@ -1,3 +1,42 @@
-from fastapi import FastAPI
+from contextlib import asynccontextmanager
+from typing import Annotated
+from database import engine
+from fastapi import Depends, FastAPI, HTTPException, Query
+from sqlmodel import Field, Session, SQLModel, create_engine, select
+from starlette.middleware.cors import CORSMiddleware
 
-app=FastAPI()
+
+
+
+def create_db_and_tables():
+    
+    SQLModel.metadata.create_all(engine)
+    print("✅ Baza i tabele su spremne!")
+
+
+@asynccontextmanager
+async def lifespan(app: FastAPI):
+    
+    create_db_and_tables()
+    yield
+    print("🛑 Gašenje aplikacije...")
+
+
+def start_application():
+    app = FastAPI(lifespan=lifespan)
+
+    origins = ["http://localhost:3000"]  # ✅ DOZVOLJENO ZA FRONTEND
+
+    app.add_middleware(
+        CORSMiddleware,
+        allow_origins=origins,
+        allow_credentials=True,
+        allow_methods=["*"],
+        allow_headers=["*"]
+    )
+
+    return app
+
+
+
+app = start_application()
